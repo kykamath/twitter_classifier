ec27bb241f64a9c673408756e0c938838e1ba1ce
kykamath <krishna.kamath@gmail.com>
2011-03-18 19:17:10 -0500

    Added expert users.

--- src/classes.py
+++ src/classes.py
@@ -5,14 +5,14 @@ Created on Mar 18, 2011
 '''
 from collections import defaultdict
 class ExpertUsers:
-    def __init__(self, number=None):
-        self.number = number
+    def __init__(self, number=1250):
+        self.number, self.users = number, defaultdict(dict)
         usersData = defaultdict(list)
-        for l in open('/data/twitter/users/crawl/users_to_crawl'):
-            data = l.strip().split()
-            usersData[data[0]].append(data[1:])
-        for k, v in usersData.iteritems(): print k, len(v)
-            
+        for l in open('/data/twitter/users/crawl/users_to_crawl'): data = l.strip().split(); usersData[data[0]].append(data[1:])
+        for k, v in usersData.iteritems(): 
+            for user in v[:self.number]: self.users[user[1]] = {'screen_name': data[0], 'class':k}
+        for k,v in self.users.iteritems(): print k, v
+        
 #                self.experts[data[2]]={'screen_name': data[1], 'class':data[0]}
 
 
