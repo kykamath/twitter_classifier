cbbdd25e876146dac04564a9cf9e2d2706254989
kykamath <krishna.kamath@gmail.com>
2011-03-18 21:52:40 -0500

    Modified module to create training documents.

--- src/build_training_set.py
+++ src/build_training_set.py
@@ -4,17 +4,19 @@ Created on Mar 18, 2011
 @author: kykamath
 '''
 from utilities import ExpertUsers, Utilities
+from settings import Settings
+
 
 class CreateDocuments:
     @staticmethod
-    def getTweetsFromExperts(expertsList):
+    def getTweetsFromExperts(expertsList, file):
         for tweet in Utilities.iterateTweetsFromGzip(file):
             if tweet['user']['id_str'] in expertsList: yield tweet
     @staticmethod
     def createTrainingSetForDifferentNumberOfUsers(length):
         experts = ExpertUsers(number=length)
         print experts.list
-        for tweet in CreateDocuments.getTweetsFromExperts(experts.list):
+        for tweet in CreateDocuments.getTweetsFromExperts(experts.list, Settings.twitterUsersTweetsFolder+'2011_3_10.gz'):
             print tweet
         
         
