b39311ecf0094c0d32a9f504c1add5938e422f82
kykamath <krishna.kamath@gmail.com>
2011-04-13 22:26:38 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -7,12 +7,16 @@ from utilities import Utilities
 from settings import DataType, Settings
 import pprint
 class DocumentFormatUnigram(object):
+    keys = ['class', 'text', 'created_at', 'id']
     def __init__(self, currentTime):
         self.currentTime = currentTime
         self.combinedSetFile = Utilities.getCombinedFile(currentTime, DataType.raw)
     def convert(self):
         for tweet in Utilities.iterateTweetsFromFile(self.combinedSetFile):
-            pprint.pprint(tweet)
+            data = {}
+            for k in DocumentFormatUnigram.keys: data[k]=tweet[k]
+            data['screen_name'] = tweet['user']['screen_name']; data['user_id'] = tweet['user']['id_str']
+            pprint.pprint(data)
             exit()
         
 class ExpertsClassifier:
