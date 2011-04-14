d9d7d43d0a97eba00367ecfb20c61d40f43dfa4d
Krishna Kamath <krishna.kamath@gmail.com>
2011-04-14 13:09:06 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -9,10 +9,21 @@ from datetime import timedelta
 from classifiers import FixedWindowClassifier, TestDocuments
 
 
+class GenerateClassifiers:
+    @staticmethod
+    def fixedWindowOfDifferentLengths(maxLength=16):
+        currentDay = Settings.startTime
+        while currentDay<=Settings.endTime:
+            classifierLengths = [1]+range(2,min([maxLength, (currentDay-Settings.startTime).days]),2)
+            print classifierLengths
+            currentDay+=timedelta(days=1)
+    
 
 if __name__ == '__main__':
-    FixedWindowClassifier(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).trainAndSave()
-    classifier = FixedWindowClassifier(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1)
-    classifier.load()
-    print 'today:', classifier.getAccuracy(TestDocuments(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
-    print 'future:', classifier.getAccuracy(TestDocuments(currentTime=Settings.startTime+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
+#    FixedWindowClassifier(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).trainAndSave()
+#    classifier = FixedWindowClassifier(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1)
+#    classifier.load()
+#    print 'today:', classifier.getAccuracy(TestDocuments(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
+#    print 'future:', classifier.getAccuracy(TestDocuments(currentTime=Settings.startTime+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
+
+    GenerateClassifiers.fixedWindowOfDifferentLengths()
