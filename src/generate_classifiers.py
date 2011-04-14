519a9dbc0a1595116f0c40956b48004185a345bd
kykamath <krishna.kamath@gmail.com>
2011-04-14 01:17:57 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -6,18 +6,22 @@ Created on Apr 13, 2011
 from utilities import Utilities
 from settings import Settings
 from datasets import DataType
-#from settings import DataType, Settings
-#import pprint
-
+from datetime import timedelta
         
 class ExpertsClassifier:
-    def __init__(self, currentTime, numberOfExperts, dataType):
+    def __init__(self, currentTime, numberOfExperts, dataType, historyLength=1):
         self.currentTime = currentTime
         self.numberOfExperts = numberOfExperts
         self.dataType = dataType
+        self.historyLength = historyLength
+        self.classifierId
+    def _iterateTrainingDocuments(self):
+        currentTime=self.currentTime
+        for i in range(self.historyLength):
+            trainingFile = Utilities.getTrainingFile(currentTime, self.dataType, self.numberOfExperts)
+            print trainingFile
+            currentTime-=timedelta(days=1)
     def train(self):
-        trainingFile = Utilities.getTrainingFile(self.currentTime, self.dataType, self.numberOfExperts)
-        print trainingFile
-    
+        self._iterateTrainingDocuments()
 if __name__ == '__main__':
     ExpertsClassifier(Settings.startTime, Settings.numberOfExperts, DataType.ruusl).train()  
