8e9065c9fb52e841a5066f5fae7f969ab562009d
Krishna Kamath <krishna.kamath@gmail.com>
2011-04-14 16:32:56 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -29,7 +29,8 @@ class AnalyzeClassifiers:
             for noOfDays in Utilities.getClassifierLengthsByDay(currentDay, maxLength): 
                 classifier = FixedWindowClassifier(currentTime=currentDay, numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=noOfDays)
                 classifier.load()
-                print currentDay, noOfDays, classifier.getAccuracy(TestDocuments(currentTime=currentDay+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
+                print currentDay, noOfDays, 'accuracy', classifier.getAccuracy(TestDocuments(currentTime=currentDay+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
+                print currentDay, noOfDays, 'aucm', classifier.getAUCM(TestDocuments(currentTime=currentDay+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.typeRuusl, noOfDays=1).iterator())
             currentDay+=timedelta(days=1)
 
 if __name__ == '__main__':
