b601d7144ab3eb9bc7c7389b29b2cc77497ad000
Krishna Kamath <krishna.kamath@gmail.com>
2011-04-14 11:21:48 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -32,6 +32,6 @@ class TestDocuments:
 if __name__ == '__main__':
 #    ExpertsClassifier(currentTime=Settings.startTime+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=2).trainAndSave()  
     i = 1
-    for d in TestDocuments(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=2).iterate():
+    for d in TestDocuments(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=4).iterate():
         print i, d[1]
         i+=1
