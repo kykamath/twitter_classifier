f2fa455f3cee44b98e56d6ed83f0af64c04aa290
kykamath <krishna.kamath@gmail.com>
2011-04-13 22:14:44 -0500

    Added document processing code.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -3,8 +3,22 @@ Created on Apr 13, 2011
 
 @author: kykamath
 '''
+from utilities import Utilities
+from settings import DataType, Settings
+class DocumentFormatUnigram(object):
+    def __init__(self, currentTime):
+        self.currentTime = currentTime
+        self.combinedSetFile = Utilities.getCombinedFile(currentTime, DataType.raw)
+    def convert(self):
+        for tweet in Utilities.iterateTweetsFromFile(self.combinedSetFile):
+            print tweet
+            exit()
+        
 class ExpertsClassifier:
     def __init__(self, numberOfExperts):
         self.numberOfExperts = numberOfExperts
+        
+if __name__ == '__main__':
+    DocumentFormatUnigram(Settings.startTime).convert()
         
     
