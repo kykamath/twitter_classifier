dc6aacd0bab688b299efcb980f6e17725affee35
Krishna Kamath <krishna.kamath@gmail.com>
2011-04-14 11:20:44 -0500

    Minor modification.

--- src/generate_classifiers.py
+++ src/generate_classifiers.py
@@ -11,38 +11,25 @@ from classes import Classifier
 
 class ExpertsClassifier(Classifier):
     def __init__(self, **kwargs):
+#        ExpertsClassifier(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=1)
         super(ExpertsClassifier, self).__init__()
-#        self.currentTime = currentTime
-#        self.numberOfExperts = numberOfExperts
-#        self.dataType = dataType
-#        self.historyLength = historyLength
         self.kwargs=kwargs
         self.trainedClassifierFile = Utilities.getTrainedClassifierFile(**kwargs)
-#    def trainingDocuments(self):
-#        currentTime=self.currentTime
-#        for i in range(self.historyLength):
-#            trainingFile = Utilities.getTrainingFile(currentTime, self.dataType, self.numberOfExperts)
-#            for tweet in Utilities.iterateTweetsFromFile(trainingFile): yield (tweet['document'], tweet['class'])
-#            currentTime-=timedelta(days=1)
     def trainAndSave(self):
         Utilities.createDirectory(self.trainedClassifierFile)
-#        for t in self.trainingDocuments(): print t[1]
-        for t in Utilities.getDocuments(fileNameMethod=Utilities.getTrainingFile, dataDirection=DataDirection.past, **self.kwargs): print t[1]
-        exit()
-        self.trainClassifier([t for t in self.trainingDocuments()])
+#        for t in Utilities.getDocuments(fileNameMethod=Utilities.getTrainingFile, dataDirection=DataDirection.past, **self.kwargs): print t[1]
+#        exit()
+        self.trainClassifier([t for t in Utilities.getDocuments(fileNameMethod=Utilities.getTrainingFile, dataDirection=DataDirection.past, **self.kwargs)])
         Classifier.saveClassifier(self.classifier, self.trainedClassifierFile)
     def load(self):
         self.classifier = Classifier.loadClassifier(self.trainedClassifierFile)
 
-#class TestDocuments:
-#    def __init__(self, currentTime, numberOfExperts, dataType, historyLength=1):
-#        self.currentTime = currentTime
-#        self.numberOfExperts = numberOfExperts
-#        self.dataType = dataType
-#        self.historyLength = historyLength
-#    @staticmethod
-#    def getTestDocuments():
-#        pass
+class TestDocuments:
+    def __init__(self, **kwargs): self.kwargs=kwargs
+    def iterate(self):
+        return Utilities.getDocuments(fileNameMethod=Utilities.getTestFile, dataDirection=DataDirection.future, bottom=True, **self.kwargs)
         
 if __name__ == '__main__':
-    ExpertsClassifier(currentTime=Settings.startTime+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=2).trainAndSave()  
+#    ExpertsClassifier(currentTime=Settings.startTime+timedelta(days=1), numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=2).trainAndSave()  
+    for d in TestDocuments(currentTime=Settings.startTime, numberOfExperts=Settings.numberOfExperts, dataType=DataType.ruusl, historyLength=2).iterate():
+        print d[1]
