a23abaf35a13937c2444f81227b1beeb6b5fe918
Krishna Kamath <krishna.kamath@gmail.com>
2011-04-15 14:11:28 -0500

    Added collocations.

--- src/utilities.py
+++ src/utilities.py
@@ -77,6 +77,8 @@ class Utilities:
         fileNameMethod=kwargs['fileNameMethod']
         del kwargs['currentTime']
         for i in range(kwargs['noOfDays']):
+            print  ' dfdsfsd  ', fileNameMethod(currentTime=currentTime, **kwargs)
+            exit()
             for tweet in Utilities.iterateTweetsFromFile(fileNameMethod(currentTime=currentTime, **kwargs)): yield (tweet['document'], tweet['class'])
             currentTime=currentTime+kwargs['dataDirection']*timedelta(days=1)
     @staticmethod
