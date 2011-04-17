907f531e4955ba186fc729f284f23a1def9a5ac9
kykamath <krishna.kamath@gmail.com>
2011-04-17 14:21:16 -0500

    Minor modification.

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ class Settings:
     twitter_api_time_format = '%a %b %d %H:%M:%S +0000 %Y'
     
     # Time data
-    startTime=datetime(2011,3,19)
+    startTime=datetime(2011,4,2)
     endTime=datetime(2011,4,12)
     
     numberOfExperts = 125
