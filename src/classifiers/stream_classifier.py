e0cbf21e59f02c6ec14a635128bd30c0b4791015
kykamath <krishna.kamath@gmail.com>
2011-04-17 22:54:36 -0500

    Minor modifications.

--- src/classifiers/stream_classifier.py
+++ src/classifiers/stream_classifier.py
@@ -11,8 +11,12 @@ from settings import Settings
 from datetime import datetime, timedelta
 
 def stream_classifier(**kwargs):
+    firstDay = Settings.startTime+timedelta(days=1)
     for tweet in Utilities.getTweets(fileNameMethod=Utilities.getStreamingSetsFile, dataDirection=DataDirection.past, completeTweets=True, **kwargs):
-        print datetime.strptime(tweet['created_at'], Settings.twitter_api_time_format).day
+        tweetTimeStamp = datetime.strptime(tweet['created_at'], Settings.twitter_api_time_format)
+        print firstDay
+        if firstDay<tweetTimeStamp:
+            print firstDay, tweetTimeStamp
     
 if __name__ == '__main__':
     stream_classifier(currentTime=Settings.startTime, dataType=DocumentType.typeRuuslUnigram, numberOfExperts=Settings.numberOfExperts, noOfDays=1)
