98028d57b7a686358177adf0ed5ea1cbb5e6b038
kykamath <krishna.kamath@gmail.com>
2011-04-17 22:51:40 -0500

    Minor modifications.

--- src/classifiers/stream_classifier.py
+++ src/classifiers/stream_classifier.py
@@ -8,10 +8,11 @@ sys.path.append('../')
 from datasets import DataDirection, DocumentType
 from utilities import Utilities
 from settings import Settings
+from datetime import datetime, timedelta
 
 def stream_classifier(**kwargs):
-    for t in Utilities.getTweets(fileNameMethod=Utilities.getStreamingSetsFile, dataDirection=DataDirection.past, completeTweets=True, **kwargs):
-        print t
+    for tweet in Utilities.getTweets(fileNameMethod=Utilities.getStreamingSetsFile, dataDirection=DataDirection.past, completeTweets=True, **kwargs):
+        print datetime.strptime(tweet['created_at'], Settings.twitter_api_time_format).day
     
 if __name__ == '__main__':
     stream_classifier(currentTime=Settings.startTime, dataType=DocumentType.typeRuuslUnigram, numberOfExperts=Settings.numberOfExperts, noOfDays=1)
