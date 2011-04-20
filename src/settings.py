ce24f4a77748fff3000d16cf2a4323516a84ea4f
kykamath <krishna.kamath@gmail.com>
2011-04-19 23:31:57 -0500

    Minor modifications.

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ class Settings:
     collocations_percentage_of_collocations_to_output = 0.01
     
     #Stream classifier.
-    stream_classifier_class_probability_threshold = 0.40
+    stream_classifier_class_probability_threshold = 0.60
     stream_classifier_decay_rate = 0.75
     
     # Experiments
